72424ab18c377d733786125d9e2448100892d633
Priyanshi Dobariya <103962186+Priyanshi-6014@users.noreply.github.com>
2022-10-03 08:30:24 -0400

    Add files via upload

---
+++ question_6.py
@@ -0,0 +1,22 @@
+
+
+
+def proportion (mylist, item):  # defining a function that counts proprotion of each item in the list.
+    count =0
+    for x in mylist:
+        if x<=int(item):         # will count how many numbers are smaller than or equal to that number in the list.
+            count+=1
+    return(round(count/len(mylist),2))      # divide by length of list to give proprotion
+
+
+
+
+def gives_dictionary(*list):         # function that will retur dictionary with key value of number and its proportion
+    for x in list:
+     print(f'{x} : {proportion(list,x)}')
+
+
+
+
+
+
